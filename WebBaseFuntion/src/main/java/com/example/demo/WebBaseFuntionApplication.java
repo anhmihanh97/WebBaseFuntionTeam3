4c26f26b470608ec1c6e2c2662b2f9d5ce236c0c
anhmihanh97 <anhmihanh97@gmail.com>
2019-01-11 10:22:38 +0700

    update

---
+++ WebBaseFuntion/src/main/java/com/example/demo/WebBaseFuntionApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBaseFuntionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebBaseFuntionApplication.class, args);
+	}
+
+}
+
